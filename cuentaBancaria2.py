d08d4cef2698e503ef89d6ee0cca21d6255261e6
atiglla <99499241+atiglla@users.noreply.github.com>
2022-03-02 16:28:49 -0500

    second commit

--- cuentaBancaria2.py
+++ cuentaBancaria2.py
@@ -3,13 +3,18 @@ class CuentaBancaria:
     def __init__(self, balance=0,tasa=0):
         self.balance=balance
         self.tasa=tasa/100
+        CuentaBancaria.cuentas.append(self)
     
     def deposito(self,amount):
         self.balance+=amount
         return self
 
     def retiro(self,amount):
-        self.balance-=amount
+        if self.balance-amount>=0:
+            self.balance-=amount
+        else:
+            print ("Fondos insuficientes: cobrando una tarifa de $5")
+            self.balance-=5
         return self
 
     def mostrar_info_cuenta (self):
@@ -22,16 +27,14 @@ class CuentaBancaria:
     
     @classmethod
     def todas (cls):
-        total=0
         for cuenta in cls.cuentas:
-            total+= cuenta.balance
-        return total
+            cuenta.mostrar_info_cuenta()
         
 cuenta1=CuentaBancaria(tasa=10)
 cuenta2=CuentaBancaria(tasa=5)
 
 cuenta1.deposito(100).deposito(200).deposito(300).retiro(100).generar_interes().mostrar_info_cuenta()
-cuenta2.deposito(200).deposito(200).retiro(50).retiro(50).retiro(50).retiro(50).generar_interes().mostrar_info_cuenta()
+cuenta2.deposito(200).deposito(200).retiro(500).retiro(50).retiro(50).retiro(50).generar_interes().mostrar_info_cuenta()
 
-print(CuentaBancaria.cuentas)
+CuentaBancaria.todas()
 
